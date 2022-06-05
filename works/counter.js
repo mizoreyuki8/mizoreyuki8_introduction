83f08099375156fb645d5185852a76dfe05d7d7e
mizoreyuki8 <yukia2th@gmail.com>
2022-06-05 12:50:25 +0900

    down button

--- works/counter.js
+++ works/counter.js
@@ -4,3 +4,8 @@ const countUp = () => {
     const countElement= document.querySelector('#count')
     countElement.innerText = '回数: ' + count
 }
+const reset = () => {
+    count--
+    const countElement= document.querySelector('#count')
+    countElement.innerText = '回数: ' + count
+}
